4416d9965a09f219016af27b244651b3bb1cb594
Pournima Khamble <pournimakhamble16@gmail.com>
2026-07-29 19:53:59 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -10,6 +10,7 @@ npm install
 echo "Building application..."
 npm run build
 
-echo "Build completed successfully."
-
+echo "Setting permissions..."
 chmod -R 755 /var/www/expense-tracker
+
+echo "Build completed successfully."
